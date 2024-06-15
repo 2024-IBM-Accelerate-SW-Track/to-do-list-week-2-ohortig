196ca8c2f1d70f6662c43ef1a30dfd4f5315d933
omerhor <omer.hortig@gmail.com>
2024-06-15 16:12:17 -0500

    Lab 2 Complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,16 @@ class Home extends Component {
       todos: [],
     };
   }
+  // delete ToDo function
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+  };
+
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -19,24 +29,28 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
-    this.setState({
+    if (this.state.todos.find(t => t === todo)) {
+      return;
+    } else {
+      todo.id = Math.random();
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+      // Update the local state with the new array.
+      this.setState({
       todos: new_list,
-    });
+      });
+    }
   };
   render() {
     return (
       <div className="Home">
-        <h1>Todo's </h1>
+        <h1>Todo's</h1>
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
-        <AddTodo addTodo={this.addTodo} />
+        <AddTodo addTodo={this.addTodo}/>
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
